f8a3101ecb034ff79ea8a0bb4c585506a744c7bc
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-03-17 01:29:11 +0530

    s c entered navigation tabs

--- src/components/Home/NavigationBar.jsx
+++ src/components/Home/NavigationBar.jsx
@@ -1,33 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Navbar, Tab, Tabs } from 'react-bootstrap'
 import ProfileBoxToggle from './ProfileBoxToggle'
 import Contact from './Contact';
+import styles from "./Home.module.css"
+import { authUser } from '../../backend/autharization';
 
-function NavigationBar({handleSigninButton}) {
+function NavigationBar({ handleSigninButton, authUser }) {
+    const [signed, setSigned] = useState('');
+
+    useEffect(() => {
+        // Update the signed state when the component mounts or authUser changes
+        handleSignedButton();
+    }, [authUser]);
+
+    const handleSignedButton = () => {
+        if (authUser) {
+            setSigned('Profile');
+        } else {
+            setSigned('Signin');
+        }
+    };
+
     return (
         <>
-            <Navbar fixed="top" >
+            <Navbar fixed="top" className={styles.naviContainer}>
                 <Container>
                     <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-                    <Navbar.Toggle />                    
+                    <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                    <Tabs>
-                        <Tab eventKey="home" title="Home">
-
-                        </Tab>
-                        <Tab eventKey="guide" title="Guide">
-
-                        </Tab>
-                        <Tab eventKey="contact" title="Contact">
-                            <Contact/>
-                        </Tab>
-                    </Tabs>
-                        <ProfileBoxToggle handleSigninButton={handleSigninButton}/>
+                        <Tabs>
+                            <Tab eventKey="home" title="Home">
+                                {/* Content for the Home tab */}
+                            </Tab>
+                            <Tab eventKey="guide" title="Guide">
+                                {/* Content for the Guide tab */}
+                            </Tab>
+                            <Tab eventKey="contact" title="Contact">
+                                <Contact />
+                            </Tab>
+                            <Tab eventKey='profile' title={signed}>
+                                <ProfileBoxToggle handleSigninButton={handleSigninButton} />
+                            </Tab>
+                        </Tabs>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
-    )
+    );
 }
 
 export default NavigationBar;
