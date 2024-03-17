0fb75ba6b51ec09f6784654c3714b2291c2d6dce
janum0613 <121988983+janum0613@users.noreply.github.com>
2024-03-17 14:16:26 +0530

    jc contact

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+      <div className="bg-white rounded w-30 p-10">
+        <h1>Our Team</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name"><strong>Name</strong></label>
+            <input type='name' placeholder='enter name' name='name'/>
+          </div>
+        </form>
+      </div>
       
     </div>
   )
