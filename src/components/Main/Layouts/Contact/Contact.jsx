c1e4558014eca29cf9e4b11311096a4cb4ead44c
janum0613 <121988983+janum0613@users.noreply.github.com>
2024-03-19 16:41:28 +0530

    jc changed

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import styles from './Contact.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { FaSquarePhone } from "react-icons/fa6";
-//import { MdEmail } from "react-icons/md";
-//import { FaAddressBook } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { IoLogoWhatsapp } from "react-icons/io";
 import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+
+
 function Contact() {
+  
+   
   return (
     <>
+    <div  className={styles.contactMap}>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251709.86090183843!2d79.79166219246085!3d9.683893700000018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe55d8c70c5db9%3A0xc4ee5d6945ad9bbd!2sUniversity%20of%20Jaffna!5e0!3m2!1sen!2slk!4v1710826610095!5m2!1sen!2slk" 
+              width="400"
+               height="450"
+                style={{ border: "0" }} 
+                allowfullscreen="" 
+                loading="lazy" 
+                ></iframe>
+            </div>
+     
       <section className={styles.sectionWrapper}>
         <div className={styles.boxWrapper}>
           <div className={styles.infoWrapper}>
@@ -18,24 +27,28 @@ function Contact() {
             <h3 className={styles.infoSubTitle}>Fill up the form and our Team will get back to you within 24 hours</h3>
             <ul className={styles.infoDetails}>
               <li>
-              <FontAwesomeIcon icon={faPhone} />
+              <i><FontAwesomeIcon icon={faPhone} /></i>
                 <span>Phone:</span>
               </li>
               <li>
-              <FontAwesomeIcon icon={faEnvelope} />
+              <i><FontAwesomeIcon icon={faEnvelope} /></i>
                 <span>Email:</span>
               </li>
               <li>
-              <FontAwesomeIcon icon={faLocationDot} />
+              <i><FontAwesomeIcon icon={faLocationDot} /></i>
                 <span>Location:</span>
               </li>
+              <li>
+              
+              
+              </li>
             </ul>
-            <ul className={styles.socialIcons}>
-              <li><i><FaFacebook /></i></li>
-              <li><i><FaGithub /></i></li>
-              <li><i><IoLogoWhatsapp /></i></li>
-            </ul>
+
+
+            
+           
           </div>
+          
 
           <div className={styles.formWrapper}>
             <form method='POST'>
@@ -53,15 +66,47 @@ function Contact() {
                 <div className={styles.formGroup}>
                   <input type="number" placeholder='Phone' />
                 </div>
+
+
+                
+                  
+                <div className={`${styles.formGroup} ${styles.radioGroup}`}>
+                <div className={styles.radioOption}>
+                  <label> Type:</label>
+                  </div>
+                  
+  <div className={styles.radioOption}>
+    
+    <input type="radio" id="feedback" name="messageType" value="feedback" />
+    <label htmlFor="feedback">Feedback</label>
+  </div>
+  <div className={styles.radioOption}>
+    <input type="radio" id="issue" name="messageType" value="issue" />
+    <label htmlFor="issue">Issue</label>
+  </div>
+</div>
+
+
+
+               
+                
+                
+                
                 <div className={styles.formGroup}>
                   <textarea name="message" placeholder='Write your message'></textarea>
                 </div>
               </div>
               <input type='submit' value="Send Message" className={styles.submitButton} />
             </form>
+           
           </div>
+          
+
         </div>
+        
       </section>
+      
+      
     </>
   );
 }
