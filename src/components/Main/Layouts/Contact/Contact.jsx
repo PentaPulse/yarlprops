a202b2e54ee9b10aeb10ab15e3bc20f235385a01
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-03-19 16:22:21 +0530

    s c contact

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -28,11 +28,11 @@ function Contact() {
   return (
     <>
       <Container className={`${styless.contactContainer} ${'mt-4'}`}>
-        <img src='/sample/profile.svg' width={200} />
-        <img src='/sample/profile.svg' width={200} />
-        <img src='/sample/profile.svg' width={200} />
-        <img src='/sample/profile.svg' width={200} />
-        <img src='/sample/profile.svg' width={200} />
+        <img src='/sample/profile.svg' width='20%' />
+        <img src='/sample/profile.svg' width='20%' />
+        <img src='/sample/profile.svg' width='20%' />
+        <img src='/sample/profile.svg' width='20%' />
+        <img src='/sample/profile.svg' width='20%' />
       </Container>
       <Container className={`${styless.contactContainer} ${'mt-4'}`}>
         <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
@@ -126,16 +126,16 @@ function Contact() {
       <Container className={`${styless.contactContainer} ${'mt-4'}`}>
         <Row>
           <Col>
-                <p>Email address</p>
+            <p>Email address</p>
           </Col>
           <Col>
-          <div style={{ height: '60vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      />
-    </div>
+            <div style={{ height: '500px', width: '100%' }}>
+              <GoogleMapReact
+                bootstrapURLKeys={{ key: "" }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+              />
+            </div>
           </Col>
         </Row>
       </Container>
