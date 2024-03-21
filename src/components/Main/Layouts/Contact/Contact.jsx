64457b19a4ab9f684b83b31016d7322f3189149d
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-03-21 21:35:53 +0530

    s c test

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -56,14 +56,16 @@ function ContactComps() {
 function ContactInfo() {
     return (
         <>
-            <h2>We are here</h2>
-            <h4>Email us : </h4>
-            <p>penta5pulse@gmail.com</p>
-            <h4>Post us : </h4>
-            <p>postal box maintaining 😁</p>
-            <h4>Locate us : </h4>
-            <div style={{ height: '500px', width: '100%' }}>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.9665147625187!2d80.02048177478142!3d9.68389897839459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe55d8c70c5db9%3A0xc4ee5d6945ad9bbd!2sUniversity%20of%20Jaffna!5e0!3m2!1sen!2slk!4v1711006328892!5m2!1sen!2slk" width='100%' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+            <div>
+                <h2>We are here</h2>
+                <h4>Email us : </h4>
+                <p>penta5pulse@gmail.com</p>
+                <h4>Post us : </h4>
+                <p>postal box maintaining 😁</p>
+                <h4>Locate us : </h4>
+                <div style={{ height: '500px', width: '100%' }}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.9665147625187!2d80.02048177478142!3d9.68389897839459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe55d8c70c5db9%3A0xc4ee5d6945ad9bbd!2sUniversity%20of%20Jaffna!5e0!3m2!1sen!2slk!4v1711006328892!5m2!1sen!2slk" width='80%' height={150} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='googleMap'/>
+                </div>
             </div>
         </>
     )
