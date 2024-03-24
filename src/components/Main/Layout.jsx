f190447856cd0d981aeea0667f74dd8e1f9d23ad
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-03-24 17:02:05 +0530

    s c changed login lib to material from bootstrap

--- src/components/Main/Layout.jsx
+++ src/components/Main/Layout.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react'
-import { Modal } from 'react-bootstrap';
+//import { Modal } from 'react-bootstrap';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
 import { Signin, Signup } from '../common/sign/Sign';
 import NavigationBar from '../common/NavigationBar/NavigationBar';
 import Footer from '../common/Footer';
 
-function Layout({children}) {
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function Layout({ children }) {
     const [open, setOpen] = useState(false);
     const [toggle, setToggle] = useState(true);
 
@@ -27,17 +43,27 @@ function Layout({children}) {
         <>
             <NavigationBar handleSigninButton={handleSigninButton} />
             <div className='d-flex justify-content-center align-items-center mt-30 text-center'>
-                <Modal placement='top' onHide={handleClose} show={open} height={100}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{toggle ? <h2>Welcome back</h2> : <h2 >Join YarlRent</h2>}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        {toggle ? <Signin toggleSignup={toggleSignup} toggle /> : <Signup toggleSignin={toggleSignin} toggle />}
-                    </Modal.Body>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    slots={{ backdrop: Backdrop }}
+                    slotProps={{
+                        backdrop: {
+                            timeout: 500,
+                        },
+                    }}>
+                    <Fade in={open}>
+                    <Box sx={style}>
+                            {toggle ? <Signin toggleSignup={toggleSignup} toggle /> : <Signup toggleSignin={toggleSignin} toggle />}
+                            </Box>
+                    </Fade>
                 </Modal>
             </div>
             <div className='mt-5'>
-            {children}
+                {children}
             </div>
             <Footer />
         </>
