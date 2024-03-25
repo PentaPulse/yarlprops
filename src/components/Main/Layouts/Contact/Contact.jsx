3234b9f60b2821bb1c5c0597833d25693bd9dfc2
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-03-25 22:40:24 +0530

    s c Login and register processes fully completed

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Contact.module.css'
 import './ContactComps.css'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Container} from 'react-bootstrap'
 
 function About() {
     return (
@@ -134,65 +134,6 @@ function ContactComps() {
             </div>
         </div>
     
-        </>
-    )
-}
-
-function ContactInfo() {
-    return (
-        <>
-            <div className={styles.contactInfo}>
-                <h2 className={styles.formTitle}>We are here</h2>
-                <h4>Email us : </h4>
-                <p>penta5pulse@gmail.com</p>
-                <h4>Post us : </h4>
-                <p>postal box maintaining 😁</p>
-                <h4>Locate us : </h4>
-                <div style={{ height: '500px', width: '100%' }}>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.9665147625187!2d80.02048177478142!3d9.68389897839459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe55d8c70c5db9%3A0xc4ee5d6945ad9bbd!2sUniversity%20of%20Jaffna!5e0!3m2!1sen!2slk!4v1711006328892!5m2!1sen!2slk" width='80%' height={150} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='googleMap' />
-                </div>
-            </div>
-        </>
-    )
-}
-
-function ContactForm() {
-    return (
-        <>
-            <div className={styles.formWrapper}>
-                <form method='POST'>
-                    <h2 className={styles.formTitle}>Feel free to contact us</h2>
-                    <div className={styles.formFields}>
-                        <Row>
-                            <Col>
-                                <div className={styles.formGroup}>
-                                    <input type="text" placeholder='First Name' />
-                                </div>
-                            </Col>
-                            <Col>
-                                <div className={styles.formGroup}>
-                                    <input type="text" placeholder='Last Name' />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <div className={styles.formEmail}>
-                                    <input type="email" placeholder='Email' />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <div className={styles.formGroup}>
-                                    <textarea name="message" placeholder='Write your message'></textarea>
-                                </div>
-                            </Col>
-                        </Row>
-                    </div>
-                    <input type='submit' value="Send Message" className={styles.submitButton} />
-                </form>
-            </div>
         </>
     )
 }
--- src/components/common/Welcome/Welcome.jsx
+++ src/components/common/Welcome/Welcome.jsx
@@ -1,10 +1,8 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { authUser } from '../../../backend/autharization';
-import { storage } from '../../../backend/storage';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { Col, FloatingLabel, Form, Row, Toast, ToastContainer } from 'react-bootstrap';
+import { FloatingLabel, Form,Toast, ToastContainer } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { Button, ButtonGroup, TextField } from '@mui/material';
 import { addUser } from '../../../backend/database';
@@ -53,8 +51,26 @@ export function Welcome({ toLogin, toRegister }) {
 }
 
 export function Login({ handleBack }) {
-    const handleLogin = () => {
-
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const handleLogin = (e) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(authUser,email,password)
+        .then((result)=>{
+            const user = result.user
+            console.log("uid : "+user.uid)
+            window.location.reload(0)
+        })
+        .catch((error)=>{
+            const errorMassege = error.massage
+            const errorCode = error.code
+            if(errorCode==="auth/invalid-email"){
+                console.log("invalid credentials")
+            }
+            else{
+            console.log(errorCode+" : "+errorMassege)
+            }
+        })
     }
     return (
         <>
@@ -62,8 +78,8 @@ export function Login({ handleBack }) {
             <h2>Login</h2>
             <hr />
             <div className="d-flex flex-column gap-4">
-            <TextField inputProps={inputStyle} label="Email" />
-            <TextField inputProps={inputStyle} label="Password" />            
+            <TextField inputProps={inputStyle} label="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            <TextField inputProps={inputStyle} label="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>            
             <span className="a">Forgot Your Password?</span>
             </div>
             <div className="text-center">
@@ -214,108 +230,3 @@ function SigninWithEmail({ handleSigninWithEmail }) {
         </>
     )
 }
-
-function SignupWithEmail({ handleSignupWithEmail }) {
-    const navigate = useNavigate();
-    const [fName, setFName] = useState('');
-    const [lName, setLName] = useState('');
-    //const [dob, setDob] = useState('');
-    const [pp, setPp] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [eye, setEye] = useState(false);
-
-    const handleFileChange = (e) => {
-        if (e.target.files[0]) {
-            setPp(e.target.files[0]);
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        createUserWithEmailAndPassword(authUser, email, password)
-            .then((result) => {
-                const user = result.user;
-                const displayName = `${fName} ${lName}`;
-                updateProfile(user, {
-                    displayName: displayName,
-                }).then(() => {
-                    if (pp) {
-                        const storageRef = ref(storage, `users/${user.uid}/profilePicture`);
-                        uploadBytes(storageRef, pp).then((snapshot) => {
-                            getDownloadURL(snapshot.ref).then((downloadURL) => {
-                                updateProfile((user), {
-                                    photoURL: downloadURL,
-                                }).then(() => {
-                                    window.location.reload(0);
-                                })
-                            })
-                        })
-                    } else {
-                        navigate('/')
-                    }
-                })
-                navigate('/')
-            }).catch((error) => {
-                const errorCode = error.code;
-                const errorMassage = error.massage;
-                console.log(errorCode + ' : ' + errorMassage)
-            })
-    }
-    const handleEye = () => {
-        setEye(!eye);
-    }
-    return (
-        <>
-            <div className='mt-3 text-center'>
-                <Form Validate onSubmit={handleSubmit}>
-                    <Row >
-                        <Col>
-                            <FloatingLabel
-                                controlId="floatingFirstName"
-                                label="First name"
-                                className="mb-3 text-center"
-                            >
-                                <Form.Control type="text" placeholder="First name" value={fName} onChange={(e) => setFName(e.target.value)} required />
-                            </FloatingLabel>
-                        </Col>
-                        <Col>
-                            <FloatingLabel
-                                controlId="floatingLastName"
-                                label="Last name"
-                                className="mb-3"
-                            >
-                                <Form.Control type="text" placeholder="Last name" value={lName} onChange={(e) => setLName(e.target.value)} required />
-                            </FloatingLabel>
-                        </Col>
-                    </Row>
-
-                    <FloatingLabel
-                        controlId="floatingProfilePicture"
-                        label="Profile picture"
-                        className="mb-3"
-                    >
-                        <Form.Control type="file" onChange={handleFileChange} />
-                    </FloatingLabel>
-
-                    <FloatingLabel
-                        controlId="floatingEmail"
-                        label="Email address"
-                        className="mb-3"
-                    >
-                        <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    </FloatingLabel>
-
-                    <FloatingLabel controlId="floatingPassword" label="Password" className='d-flex'>
-                        <Form.Control type={!eye ? "password" : "text"} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <img src={eye ? 'show/show.svg' : 'show/hide.svg'} alt='show/hide' onClick={handleEye} width={60} className='btn border' />
-                    </FloatingLabel>
-                    <Button className='mt-3 text-center' variant='dark' type='submit'>Sign up</Button>
-                </Form>
-                <br />
-                <button className='btn mt-3 mb-3' onClick={handleSignupWithEmail}><h5>Back</h5></button>
-            </div>
-        </>
-    )
-}
-
