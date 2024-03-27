369a1914ada2a407f5d4a46fcc206211098793cc
Punsisi00 <johnburns3500@gmail.com>
2024-03-27 19:02:50 +0500

    changed some contents on home.jsx

--- src/components/Main/Dashboards/Home.jsx
+++ src/components/Main/Dashboards/Home.jsx
@@ -66,28 +66,28 @@ function Home() {
                     <h3>PRODUCTS</h3>
                     <BsFillArchiveFill className='card_icon'/>
                 </div>
-                <h1>300</h1>
+                <h1>150</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
                     <h3>CATEGORIES</h3>
                     <BsFillGrid3X3GapFill className='card_icon'/>
                 </div>
-                <h1>12</h1>
+                <h1>10</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
                     <h3>CUSTOMERS</h3>
                     <BsPeopleFill className='card_icon'/>
                 </div>
-                <h1>33</h1>
+                <h1>200</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
                     <h3>ALERTS</h3>
                     <BsFillBellFill className='card_icon'/>
                 </div>
-                <h1>42</h1>
+                <h1>20</h1>
             </div>
         </div>
 
