0cdc6fb7b61aba7f8276ff937c3beb8c25d590b8
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-03-28 00:12:17 +0530

    s c test

--- src/components/Main/Profiles/UserProfile.jsx
+++ src/components/Main/Profiles/UserProfile.jsx
@@ -1,38 +1,37 @@
 import React from 'react';
-import { authUser} from '../../../backend/autharization';
+import { authUser } from '../../../backend/autharization';
 import { Col, Form, Row, Accordion, Card, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
+import { useCookies } from 'react-cookie';
 
 function Photos() {
+  const [cookies,setCookie]=useCookies(['uid','photo','dpn','email','phone'])
   const navigate = useNavigate()
 
-  let uid, photo, displayName, email, phone;
-
-  onAuthStateChanged(authUser, (user) => {
-    if (user) {
-      uid = user.uid
-      photo = user.photoURL
-      displayName = user.displayName
-      email = user.email
-      phone = user.phoneNumber
-    }
-    else {
-      navigate('/')
-    }
-  })
+  const user = authUser.currentUser
+  if (user) {
+    setCookie('uid',user.uid)
+    setCookie('photo',user.photoURL)
+    setCookie('dpn',user.displayName)
+    setCookie('email',user.email)
+    setCookie('phone',user.phoneNumber)
+    console.log(cookies)
+  }
+  else {
+    navigate('/')
+  }
   return (
     <Container className='d-flex flex-column align-items-center flex-wrap'>
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '140px' }}>
+      <div className='d-flex align-items-center' style={{ marginTop: '140px' }}>
         <div>
-          <img src={photo} alt='pic' width='40%' style={{ borderRadius: '100%', width: '90%', height: 'auto' }} />
+          <img src={cookies.photo} alt='pic' width='40%' style={{ borderRadius: '100%', width: '90%', height: 'auto' }} />
         </div>
         <div style={{ marginLeft: 'auto' }}>
           <Card border="dark" style={{ width: '50rem' }}>
             <Card.Body>
-              <Card.Title><h1>{displayName || 'Display name'}</h1></Card.Title>
-              <Card.Body><h4>{email || 'Your email'}</h4></Card.Body>
-              <Card.Body><h4>{phone || 'Your phonenumber'}</h4></Card.Body>
+              <Card.Title><h1>{cookies.dpn || 'Display name'}</h1></Card.Title>
+              <Card.Body><h4>{cookies.email || 'Your email'}</h4></Card.Body>
+              <Card.Body><h4>{cookies.phone || 'Your phonenumber'}</h4></Card.Body>
             </Card.Body>
           </Card>
         </div>
