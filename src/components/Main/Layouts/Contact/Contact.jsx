e889ef3c2143f0094dc2663a3ded982589460ad5
janum0613 <121988983+janum0613@users.noreply.github.com>
2024-04-08 21:02:28 +0530

    jc changed

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
             backgroundPosition:"center",
             backgroundSize:"cover",
             height:600,
-            width:{xs:"100%",sm:"100%",md:"100%"},
+            width:"100%",
             display:"flex",
             justifyContent:"center",
             
