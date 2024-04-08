01482e902f92e4291f22b02f9e1101517ab90b21
janum0613 <121988983+janum0613@users.noreply.github.com>
2024-04-08 20:32:43 +0530

    jc changed

--- src/components/Main/Layouts/Contact/Contact.jsx
+++ src/components/Main/Layouts/Contact/Contact.jsx
@@ -26,14 +26,14 @@ function Contact() {
                 padding:{xs:3,sm:2,md:20},
             }}
             >
-                <Box sx={{background:"white",opacity:"0.8",padding:{xs:3}}}>
+                <Box sx={{background:"white",opacity:"0.9",padding:{xs:3}}}>
                 <Typography variant={"h1"} color="black" align="center" pt={8}>
               <b>Contact Us</b>
             </Typography>
 
-            <Typography variant="body1" align="center"  pb={8}>
+            <Typography variant="body1" color="black" align="center"  pb={8}>
             We are committed to supporting you, first and foremost! We continually strive to exceed expectations and deliver superior 24/7 Days Support.
-         You can always count on us for live help whenever you need assistance. Call us now!
+         You can always count on us for live help whenever you need assistance.Call us now!
             </Typography>
 
                 </Box>
