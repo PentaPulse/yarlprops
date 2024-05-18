fdeb2a1412f433baccc996d619eb0c3bd792a325
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-05-19 01:19:55 +0530

    s c dashboard path access method added for admins

--- src/components/Routings.jsx
+++ src/components/Routings.jsx
@@ -7,25 +7,37 @@ import Contact from './Main/Layouts/Contact/Contact';
 import UserProfile from './Main/Profiles/UserProfile';
 import Admin from './Main/Dashboards/Admin1';
 import ProductPage from './Main/Layouts/Home/ProView/ProductPage';
+import { authUser } from '../backend/autharization';
 
-function Routings({handleMode}) {
-  
-  
+function Routings({ handleMode }) {
+
+  if (authUser.currentUser) {
+    return (
+      <>
+        <Router>
+          <Layout handleMode={handleMode}>
+            <Routes>
+              <Route path='/admin' element={<Admin />} />
+              <Route exact path='/profile' element={<UserProfile />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </>
+    )
+  }
   return (
     <>
-    <Router>
-      <Layout handleMode={handleMode}>
-        <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/home' element={<Home/>}/>
-          <Route path='/guide' element={<Guide/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route exact path='/profile' element={<UserProfile/>}/>
-          <Route path='/admin' element={<Admin/>}/>
-          <Route path="/product/:id" element={<ProductPage/>} />
-        </Routes>
-      </Layout>
-    </Router>
+      <Router>
+        <Layout handleMode={handleMode}>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/home' element={<Home />} />
+            <Route path='/guide' element={<Guide />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+        </Layout>
+      </Router>
     </>
   )
 }
