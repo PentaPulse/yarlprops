5acb6c14cf7f70e10df18ca8e0380c3c58fd9ce7
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-05-30 14:33:59 +0530

    s c search completed

--- src/components/Main/Layouts/Home/Home.jsx
+++ src/components/Main/Layouts/Home/Home.jsx
@@ -2,13 +2,10 @@ import * as React from 'react';
 import Slidshow from './Slideshow/Slidshow';
 import Categories from './Filters/Categories';
 import ProductsContents from './ProContent/ProductsContents';
-import SearchIcon from '@mui/icons-material/Search';
-import { Divider, Grid, List, ListItem, ListItemText, Pagination, TextField } from '@mui/material';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { db } from '../../../../backend/db/products';
+import { Divider, Grid, Pagination, TextField } from '@mui/material';
 
 function Home() {
-
+  const [searchTerm, setSearchTerm] = React.useState('');
   return (
     <>
       <Grid container justifyContent='center' spacing={3} columns={12}>
@@ -17,14 +14,20 @@ function Home() {
         </Grid>
         <Divider />
         <Grid item xs={11} sm={11} md={11} lg={11}>
-          <Search />
+          <TextField
+            label="Search"
+            variant="outlined"
+            fullWidth
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </Grid>
         <Grid item md={3}>
           <Categories />
         </Grid>
         <Grid item md={9}>
-          <ProductsContents />
-          <Pagination sx={{width:'100%'}} count={10} />
+          <ProductsContents searchTerm={searchTerm}/>
+          <Pagination sx={{ width: '100%' }} count={10} />
         </Grid>
       </Grid>
     </>
@@ -35,38 +38,15 @@ export default Home;
 
 function Search() {
   const [searchTerm, setSearchTerm] = React.useState('');
-  const [results, setResults] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      if (searchTerm) {
-        const q = query(collection(db, 'products'), where('name', '>=', searchTerm), where('name', '<=', searchTerm + '\uf8ff'));
-        const querySnapshot = await getDocs(q);
-        const items = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setResults(items);
-      } else {
-        setResults([]);
-      }
-    };
-
-    fetchData();
-  }, [searchTerm]);
   return (
     <>
-    <TextField
+      <TextField
         label="Search"
         variant="outlined"
         fullWidth
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-      />      
-      <List>
-        {results.map((result) => (
-          <ListItem key={result.id}>
-            <ListItemText primary={result.name} />
-          </ListItem>
-        ))}
-      </List>
+      />
     </>
   )
 }
--- src/components/Main/Layouts/Home/ProContent/ProductsContents.jsx
+++ src/components/Main/Layouts/Home/ProContent/ProductsContents.jsx
@@ -1,50 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { fetchProducts } from '../../../../../backend/db/products';
+import * as React from 'react';
+import { db, fetchProducts } from '../../../../../backend/db/products';
 import { Button, Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 
-const ProductsContents = () => {
-    const [products, setProducts] = useState([]);
+const ProductsContents = ({ searchTerm }) => {
+    const [products, setProducts] = React.useState([]);
     const navigate = useNavigate()
-    useEffect(() => {
+    React.useEffect(() => {
         const getProducts = async () => {
             const productList = await fetchProducts();
             setProducts(productList);
         };
         getProducts();
-    }, []);
+        const fetchData = async () => {
+            if (searchTerm) {
+                const q = query(collection(db, 'products'), where('name', '>=', searchTerm), where('name', '<=', searchTerm + '\uf8ff'));
+                const querySnapshot = await getDocs(q);
+                const items = querySnapshot.docs.map(doc => doc.data());
+                setProducts(items);
+            } else {
+                getProducts()
+            }
+        };
+
+        fetchData();
+    }, [searchTerm]);
 
     const handleCardClick = (e) => {
         navigate(`/product/${e}`)
     }
     return (
         <>
-            <section id='products' className='d-fiex justify-content-center align-items-center mt-5'>
-                <Container fixed>
-                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 3, lg: 3 }}>
-                        {products.map((product, index) => (
-                            <Grid item xs={1} sm={1} md={1} lg={1} key={index}>
-                                <Card>
-                                    <CardMedia
-                                        sx={{ height: '20rem' }}
-                                        image={product.image || 'https://picsum.photos/id/11/200/300'}
-                                        title={product.name}
-                                    />
-                                    <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                                        <Typography gutterBottom variant='h4' component='div'>
-                                            {product.name}
-                                        </Typography>
-                                        <Button onClick={() => handleCardClick(product.pid)} size="medium">
-                                            Read More
-                                        </Button>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </section>
+
+            <Container fixed>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2, md: 3, lg: 3 }}>
+                    {products.map((product, index) => (
+                        <Grid item xs={1} sm={1} md={1} lg={1} key={index}>
+                            <Card>
+                                <CardMedia
+                                    sx={{ height: '20rem' }}
+                                    image={product.image || 'https://picsum.photos/id/11/200/300'}
+                                    title={product.name}
+                                />
+                                <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <Typography gutterBottom variant='h4' component='div'>
+                                        {product.name}
+                                    </Typography>
+                                    <Button onClick={() => handleCardClick(product.pid)} size="medium">
+                                        Read More
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </>
     );
 }
