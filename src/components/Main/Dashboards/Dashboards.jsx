07ab17afee16650bbcd479bfd97b044511811a97
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-06-15 21:54:51 +0530

    test

--- src/components/Main/Dashboards/Dashboards.jsx
+++ src/components/Main/Dashboards/Dashboards.jsx
@@ -30,9 +30,9 @@ import { MaterialUISwitch, ProfileBox } from '../../common/NavigationBar/Navigat
 
 //menus
 const adminMenu = [['Overview', 'drawer/overview.svg'], ['Admins', 'drawer/admins.svg'], ['Sellers', 'drawer/seller.svg'], ['Renters', 'drawer/renter.svg'], ['Users', 'drawer/users.svg'], ['Products', 'drawer/products.svg'], ['Contact us requests', 'drawer/contactus.svg'], ['My Profile', 'drawer/profile.svg'], ['Back to Home', 'drawer/backhome.svg']]
-const sellerMenu = [['Overview', 'drawer/overview.svg'], 'Products', 'Orders', 'My Profile', 'Back to Home']
-const renterMenu = [['Overview', 'drawer/overview.svg'], 'Products', 'Orders', 'My Profile', 'Back to Home']
-const userMenu = [['Overview', 'drawer/overview.svg'], 'My Profile', 'Back to Home']
+const sellerMenu = [['Overview', 'drawer/overview.svg'], ['Products', 'drawer/products.svg'], ['Orders','drawer/orders.svg'], ['My Profile','drawer/profile.svg'], ['Back to Home', 'drawer/backhome.svg']]
+const renterMenu = [['Overview', 'drawer/overview.svg'], ['Products', 'drawer/products.svg'], ['Orders','drawer/orders.svg'], ['My Profile','drawer/profile.svg'], ['Back to Home', 'drawer/backhome.svg']]
+const userMenu = [['Overview', 'drawer/overview.svg'], 'My Profile', ['Back to Home', 'drawer/backhome.svg']]
 //boards
 const adminBoard = [<AdminOverview />, <Admins />, <AdminSellers />, <AdminRenters />, <AdminUsers />, <AdminProducts />, <ContactusReqs />, <Profile />]
 const sellerBoard = [<SellerOverview />, <SellerOrders />, <SellerProducts />, <Profile />]
@@ -107,7 +107,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 export default function Dashboards({ handleMode }) {
     const [board, setBoard] = React.useState(0)
-    const { user, home } = useAuth()
+    const { user } = useAuth()
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
 
@@ -187,7 +187,7 @@ export default function Dashboards({ handleMode }) {
                                             justifyContent: 'center',
                                         }}
                                     >
-                                        <Avatar alt={text[0]} src={text[1]} variant='square' />
+                                        <Avatar sx={{width:24,height:24}} alt={text[0]} src={text[1]} variant='square' />
                                     </ListItemIcon>
                                     <ListItemText primary={text[0]} sx={{ opacity: open ? 1 : 0 }} onClick={() => setBoard(index)} />
                                 </ListItemButton>
