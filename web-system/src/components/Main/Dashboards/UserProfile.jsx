a3bdae6ff010fde852686ab85246223c9cf0cc43
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-07-01 02:58:54 +0530

    s c test

--- web-system/src/components/Main/Dashboards/UserProfile.jsx
+++ web-system/src/components/Main/Dashboards/UserProfile.jsx
@@ -1,10 +1,19 @@
-import { Avatar, Badge, Grid, TextField } from '@mui/material';
+import { AppBar, Avatar, Badge, Box, Grid, Tab, Tabs, TextField, Typography } from '@mui/material';
 import * as React from 'react';
 import { useAuth } from '../../../backend/AuthContext';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import PropTypes from 'prop-types';
+//import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@emotion/react';
 
 export default function Profile() {
   const { user } = useAuth()
+  const theme = useTheme()
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <>
@@ -37,7 +46,53 @@ export default function Profile() {
             />
           </Grid>
         </Grid>
+        <Grid item width='100%'>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+            >
+              <Tab label="Item One" />
+              <Tab label="Item Two" />
+            </Tabs>
+          </AppBar>
+          <TabPanel value={value} index={0} >
+            Item One
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            Item Two
+          </TabPanel>
+        </Grid>
       </Grid>
     </>
   );
 }
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
