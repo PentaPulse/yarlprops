282a12522dd058e867faf1ff0709a062eae1e815
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-07-06 00:27:17 +0530

    Profile settings frontend part done

--- web-system/src/components/Main/Dashboards/UserProfile.jsx
+++ web-system/src/components/Main/Dashboards/UserProfile.jsx
@@ -1,11 +1,8 @@
-import { Accordion, AccordionDetails, AccordionSummary, Avatar, Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
-import * as React from 'react';
-import { useAuth } from '../../../backend/AuthContext';
-import PropTypes from 'prop-types';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Fade from '@mui/material/Fade';
+import React from 'react';
+import { useAuth } from '../../../backend/AuthContext'; // Update with the correct path
+import { Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Avatar, Paper, Typography } from '@mui/material';
 
-export default function Profile() {
+const Profile = () => {
   const { user } = useAuth();
   const [expanded, setExpanded] = React.useState('panel1');
 
@@ -14,241 +11,209 @@ export default function Profile() {
   };
 
   return (
-    <>
-      <Grid container spacing={2} columns={12} mt={2}>
-        <Grid container justifyContent="center" alignItems="center" >
-          <Avatar alt="Profile Picture" src={user.photoUrl || sessionStorage.getItem('pp')} sx={{ width: 150, height: 150 }} />
-          <Grid item>
-            <TextField
-              label="Name"
-              defaultValue={user.displayName}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              required
-              label="Email"
-              defaultValue={user.email || 'example@p5p.lk'}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              required
-              label="Phone Number"
-              defaultValue={user.phone || '+94 12 345 6789'}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Accordion
-            expanded={expanded === 'panel1'}
-            onChange={handleExpansion('panel1')}
-            TransitionComponent={Fade}
-            transitionDuration={400}
-            sx={{
-              '& .MuiAccordionSummary-content': { height: expanded === 'panel1' ? 'auto' : 0 },
-              '& .MuiAccordionDetails-root': { display: expanded === 'panel1' ? 'block' : 'none' },
+    <Grid container spacing={2} mt={2}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Avatar alt="Profile Picture" src={user.photoUrl || sessionStorage.getItem('pp')} sx={{ width: 150, height: 150 }} />
+        <Grid item>
+          <TextField
+            label="Name"
+            defaultValue={user.displayName}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              readOnly: true,
             }}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1-content"
-              id="panel1-header"
-            >
-              Profile Settings
-            </AccordionSummary>
-            <AccordionDetails>
-              <ProfileSettings />
-            </AccordionDetails>
-          </Accordion>
-          <Accordion
-            expanded={expanded === 'panel2'}
-            onChange={handleExpansion('panel2')}
-            TransitionComponent={Fade}
-            transitionDuration={400}
-            sx={{
-              '& .MuiAccordionSummary-content': { height: expanded === 'panel2' ? 'auto' : 0 },
-              '& .MuiAccordionDetails-root': { display: expanded === 'panel2' ? 'block' : 'none' },
+          />
+          <TextField
+            label="Email"
+            defaultValue={user.email || 'example@p5p.lk'}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              readOnly: true,
             }}
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2-content"
-              id="panel2-header"
-            >
-              Account Settings
-            </AccordionSummary>
-            <AccordionDetails>
-              <AccountSettings />
-            </AccordionDetails>
-          </Accordion>
+          />
+          <TextField
+            label="Phone Number"
+            defaultValue={user.phone || '+94 12 345 6789'}
+            fullWidth
+            margin="normal"
+            InputProps={{
+              readOnly: true,
+            }}
+          />
         </Grid>
       </Grid>
-    </>
+      <Grid item xs={12}>
+        <Paper sx={{ mt: 1, padding: '20px' }}>
+          <ProfileSettings />
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+      <Paper sx={{ mt: 1, padding: '20px' }}>
+        <AccountSettings />
+        </Paper>
+      </Grid>
+    </Grid>
   );
-}
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      gender="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
 };
 
-//Profile settings
 const ProfileSettings = () => {
   const { user } = useAuth();
   const [gender, setGender] = React.useState(user.gender || '');
-  const [edit, setEdit] = React.useState(false)
-
+  const [edit, setEdit] = React.useState(false);
 
   const handleSelectChange = (e) => {
     setGender(e.target.value);
   };
 
   const handleEditProfile = () => {
-    setEdit(true)
-  }
+    setEdit(true);
+  };
 
   const handleSubmit = () => {
-
-  }
+    setEdit(false);
+  };
 
   const handleCancel = () => {
-    setEdit(false)
-  }
+    setEdit(false);
+  };
+  const width = '35vw'
 
   return (
     <>
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '35vw' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-          <TextField
-            label="First name"
-            defaultValue={user.fname}
-            InputProps={{
-              readOnly: !edit
-            }}
-          />
-          <TextField
-            label="Last name"
-            defaultValue={user.lname}
-            InputProps={{
-              readOnly: !edit
-            }}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Email"
-            defaultValue={user.email}
-            InputProps={{
-              readOnly: !edit
-            }}
-          />
-          <TextField
-            label="Phone number"
-            defaultValue={user.phoneNumber}
-            InputProps={{
-              readOnly: !edit
-            }}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Date of Birth"
-            type="date"
-            defaultValue={user.dob} // Assuming user.dob is in YYYY-MM-DD format
-            InputLabelProps={{
-              shrink: true,
-            }}
-            InputProps={{
-              readOnly: !edit,
-            }}
-          />
-          <TextField
-            label="My role"
-            defaultValue={user.role}
-            InputProps={{
-              readOnly: true
-            }}
-          />
-        </div>
-        <div>
-          <TextField
-            label="Address"
-            defaultValue={user.address}
-            InputProps={{
-              readOnly: !edit
-            }}
-          />
-          <FormControl sx={{ m: 1, width: '35vw' }} disabled={!edit}>
-            <InputLabel>Gender</InputLabel>
-            <Select value={gender} onChange={handleSelectChange}>
-              {['', 'Male', 'Female'].map((genderOption, index) => (
-                <MenuItem key={index} value={genderOption}>{genderOption}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </div>
-        <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-          {!edit ?
-            <Button variant="contained" color="primary" onClick={handleEditProfile}>Edit Profile</Button>
-            :
-            <>
-              <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
-              <Button variant="contained" color="primary" onClick={handleCancel}>Cancel</Button>
-            </>}
-        </Container>
-      </Box>
+    <Typography>Profile Settings</Typography>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: width },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          label="First name"
+          defaultValue={user.fname}
+          InputProps={{
+            readOnly: !edit,
+          }}
+        />
+        <TextField
+          label="Last name"
+          defaultValue={user.lname}
+          InputProps={{
+            readOnly: !edit,
+          }}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Email"
+          defaultValue={user.email}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          label="Phone number"
+          defaultValue={user.phoneNumber}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Date of Birth"
+          type="date"
+          defaultValue={user.dob} // Assuming user.dob is in YYYY-MM-DD format
+          InputLabelProps={{
+            shrink: true,
+          }}
+          InputProps={{
+            readOnly: !edit,
+          }}
+        />
+        <TextField
+          label="My role"
+          defaultValue={user.role}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+      <div>
+        <TextField
+          label="Address"
+          defaultValue={user.address}
+          InputProps={{
+            readOnly: !edit,
+          }}
+        />
+        <FormControl sx={{ m: 1, width: width }} disabled={!edit}>
+          <InputLabel>Gender</InputLabel>
+          <Select value={gender} onChange={handleSelectChange}>
+            {['', 'Male', 'Female'].map((genderOption, index) => (
+              <MenuItem key={index} value={genderOption}>{genderOption}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        {!edit ? (
+          <Button variant="contained" color="primary" onClick={handleEditProfile}>
+            Edit Profile
+          </Button>
+        ) : (
+          <>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              Submit
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </>
+        )}
+      </Container>
+    </Box>
     </>
   );
 };
 
 const AccountSettings = () => {
+  const { user } = useAuth();
+
   return (
     <>
-      <Typography>Change Password</Typography>
-      <TextField className="w-30" label="Old password" />
-      <TextField className="w-30" label="New password" />
-      <TextField className="w-30" label="Confirm password" />
-      <Button>Change password</Button>
+    <Typography>Account Settings</Typography>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '40vw' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          label="First name"
+          defaultValue={user.fname}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          label="Last name"
+          defaultValue={user.lname}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+    </Box>
     </>
-  )
-}
+  );
+};
+
+export default Profile;
