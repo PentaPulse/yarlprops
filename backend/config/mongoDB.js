f81cdf465205b30004f9979d4e78c83b23d3a0ec
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-08-09 02:04:02 +0530

    local db

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -2,7 +2,8 @@ const mongoose=require('mongoose');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect("mongodb+srv://Cluster33761:X2dxeXpaUktf@cluster33761.q1ofbfy.mongodb.net/yarlprops?retryWrites=true&w=majority");
+        //cloud "mongodb+srv://Cluster33761:X2dxeXpaUktf@cluster33761.q1ofbfy.mongodb.net/yarlprops?retryWrites=true&w=majority"
+        await mongoose.connect("mongodb://127.0.0.1:27017/yarlprops");
         console.log("Mongodb connected")
     }catch(e){
         console.error(e.message);
