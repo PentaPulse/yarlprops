0520a8d32ecc54e694b26ca88fd6a555564026d4
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-08-09 17:44:40 +0530

    s c backend minor changes

--- backend/controllers/contactusController.js
+++ backend/controllers/contactusController.js
@@ -10,5 +10,18 @@ exports.getContactusResponses = async (req, res) => {
 };
 
 exports.sendContactusRequest = async(req,res)=>{
-    
+    const {firstName,lastName,email,message}=req.body;
+    try{
+      const newReq = new Contactus({
+        firstName,
+        lastName,
+        email,
+        message,
+        status:"new"
+      });
+      const response = await newReq.save()
+      res.status(200).json({code:"contactus/send-ok",message:"Message sent successfully",response})
+    }catch(error){
+      res.status(500).json({code:"contactus/send-error",message:"Message not sent",error})
+    }
 }
--- backend/routes/contactusRoutes.js
+++ backend/routes/contactusRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getContactusResponses } = require('../controllers/contactusController');
+const { getContactusResponses, sendContactusRequest } = require('../controllers/contactusController');
 const router = express.Router();
 
 router.get('/responses',getContactusResponses);
+router.post('/send',sendContactusRequest);
 
 module.exports = router;
