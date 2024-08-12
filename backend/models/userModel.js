a00218728fb0aff256cb15d68092ba12e248fec8
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-08-12 14:49:51 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     role: { type: String, required: true },
     password: { type: String ,required:true},
     myProducts:{type:Array},
-    myRents:{type:Array},
+    myRentals:{type:Array},
     myServices:{type:Array}
 });
 
