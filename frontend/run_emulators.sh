0d34ac063513528ff3593d1dcb27b30e780f8415
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-09-04 01:30:37 +0530

    s c test

--- frontend/run_emulators.sh
+++ frontend/run_emulators.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Define paths
-EMULATOR_DATA_DIR="./emulator-data"
-SCRIPT_PATH="./your-script.sh"
+# Get the Git user email
+GIT_USER_EMAIL=$(git config user.email)
+
+# Define the import directory based on the Git user email
+if [ "$GIT_USER_EMAIL" = "tahrindu.sachintha.xyz1@gmail.com" ]; then
+    IMPORT_DIR="./datas"
+else
+    IMPORT_DIR="./emulator-data/$GIT_USER_EMAIL"
+fi
+
+# Define the export directory based on Git user email
+EXPORT_DIR="./emulator-data/$GIT_USER_EMAIL"
 
 # Function to start the emulators with data import
 start_emulators() {
-    echo "Starting Firebase emulators with data import..."
-    firebase emulators:start --import="$EMULATOR_DATA_DIR"
+    echo "Starting Firebase emulators with data import from $IMPORT_DIR..."
+    firebase emulators:start --import="$IMPORT_DIR"
 }
 
 # Function to export the emulator data
 export_emulators() {
-    echo "Exporting Firebase emulators data..."
-    firebase emulators:export "$EMULATOR_DATA_DIR"
-}
-
-# Function to stop the emulators after executing a script
-stop_emulators() {
-    echo "Stopping Firebase emulators and running script..."
-    firebase emulators:exec "$SCRIPT_PATH"
+    echo "Exporting Firebase emulators data to $EXPORT_DIR..."
+    firebase emulators:export "$EXPORT_DIR"
 }
 
 # Main script execution
@@ -30,11 +33,8 @@ case $1 in
     export)
         export_emulators
         ;;
-    stop)
-        stop_emulators
-        ;;
     *)
-        echo "Usage: $0 {start|export|stop}"
+        echo "Usage: $0 {start|export}"
         exit 1
         ;;
 esac
