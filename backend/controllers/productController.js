354909fab786a5fdf8537df86126ea0da06aa7ba
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-09-15 09:32:53 +0530

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,31 +11,31 @@ exports.getProducts = async (req, res) => {
 };
 
 const addProduct = async (req, res) => {
-  try {
-      const { pid, mid, title, category, subCategory, description, location, quantity, status, images, timestamp } = req.body;
-      
-      // Create a new product instance
-      const product = new Product({
-          pid,
-          mid,
-          title,
-          category,
-          subCategory,
-          description,
-          location,
-          quantity,
-          status,
-          images,
-          timestamp
-      });
+    try {
+        const { pid, mid, title, category, subCategory, description, location, quantity, status, images, timestamp } = req.body;
+        
+        // Create a new product instance
+        const product = new Product({
+            pid,
+            mid,
+            title,
+            category,
+            subCategory,
+            description,
+            location,
+            quantity,
+            status,
+            images,
+            timestamp
+        });
 
-      // Save the product to the database
-      await product.save();
+        // Save the product to the database
+        await product.save();
 
-      res.status(201).json({ message: 'Product added successfully', product });
-  } catch (err) {
-      res.status(500).json({ error: 'Error adding product', details: err });
-  }
+        res.status(201).json({ message: 'Product added successfully', product });
+    } catch (err) {
+        res.status(500).json({ error: 'Error adding product', details: err });
+    }
 };
 
 exports.dummy = async(req,res)=>{
