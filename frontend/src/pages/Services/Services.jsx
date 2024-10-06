27ed52ca9946073a418860b5df6a61891147860e
charithgaya <121884110+charithgaya@users.noreply.github.com>
2024-10-06 16:14:19 +0530

    Styled HOME

--- frontend/src/pages/Services/Services.jsx
+++ frontend/src/pages/Services/Services.jsx
@@ -222,6 +222,7 @@ export function ServicePage() {
                             height: '100%',
                             display: 'flex',
                             flexDirection: 'column',
+                            justifyContent: 'space-between',
                             boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
                         }}>
                         <CardContent sx={{ my: '30px' }}>
