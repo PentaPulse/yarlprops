5c37f1a9ff159f5a2b6c9543aa9860b044e899ff
charithgaya <121884110+charithgaya@users.noreply.github.com>
2024-10-19 17:01:32 +0530

    fgfgfgf

--- main-app/src/pages/Home/Slideshow/Slidshow.jsx
+++ main-app/src/pages/Home/Slideshow/Slidshow.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useMediaQuery } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './SlideShow.css';
 
 function SlideShow() {
@@ -8,7 +9,7 @@ function SlideShow() {
   const isMediumScreen = useMediaQuery('(max-width:960px)');
 
   const carouselHeight = isSmallScreen ? '45vh' : isMediumScreen ? '55vh' : '75vh';
-  const FontSize = isSmallScreen ? '1.0rem' : isMediumScreen ? '1.3rem' : '1.6rem';
+  const FontSize = isSmallScreen ? '0.8rem' : isMediumScreen ? '1.0rem' : '1.2rem';
 
   return (
    <Carousel>
@@ -49,18 +50,64 @@ function SlideShow() {
             }}>
             MOTORBIKES FOR RENT
           </Typography> */}
-          <h3 style={{ fontSize: FontSize, fontWeight: 'bold', textAlign: 'center', lineHeight: 1.2 }}>MOTORBIKES FOR RENT</h3>
+          {/* <h3 style={{ fontSize: FontSize, fontWeight: 'bold', textAlign: 'center', lineHeight: 1.2 }}>MOTORBIKES FOR RENT</h3> */}
           {/* <p style={{ fontSize: FontSize, lineHeight: 1.2, fontStyle: 'italic' }}>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+          <Link to="/p/rentals">
+            <button 
+              className='bounce-button'
+              style={{ 
+                fontSize: FontSize
+              }}>
+              View Details
+            </button>
+          </Link>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <video style={{ width: '100%', height: carouselHeight }} className='d-block w-100' controls autoPlay loop muted>
+        <img style={{ height: carouselHeight }} className="d-block w-100" src="/slideshow/taxiservice.jpg" alt="Fourth slide" />
+        <Carousel.Caption>
+          {/* <Typography 
+            varient='h3' 
+            sx={{ 
+              fontWeight: 'bold',
+              textAlign: 'center',
+              fontSize: { xs:'0.8rem', sm:'1rem', md:'1.2rem', lg:'1.4rem' },
+              lineHeight: 1.2,
+            }}>
+            MOTORBIKES FOR RENT
+          </Typography> */}
+          {/* <h3 style={{ fontSize: FontSize, fontWeight: 'bold', textAlign: 'center', lineHeight: 1.2 }}>TAXI SERVICE</h3> */}
+          {/* <p style={{ fontSize: FontSize, lineHeight: 1.2, fontStyle: 'italic' }}>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+          <Link to="/p/services">
+            <button
+              className='bounce-button'
+              style={{ 
+                fontSize: FontSize
+              }}>
+              View Details
+            </button>
+          </Link>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <video style={{ height: carouselHeight }} className='d-block w-100' controls autoPlay loop muted>
             <source src="/slideshow/KKSALOON.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
         </video>
         <Carousel.Caption>
-          <h3 style={{ fontSize: FontSize, fontWeight: 'bold', textAlign: 'center', lineHeight: 1.2 }}>KK SALOON</h3>
-          <p style={{ fontSize: FontSize, lineHeight: 1.2, fontStyle: 'italic' }}>Make your appointment.</p>
+          {/* <h3 style={{ fontSize: FontSize, fontWeight: 'bold', textAlign: 'center', lineHeight: 1.0 }}>KK SALOON</h3>
+          <p style={{ fontSize: FontSize, lineHeight: 1.0, fontStyle: 'italic' }}>Make your appointment.</p> */}
+          <Link to="/p/services">
+            <button
+              className='bounce-button'
+              style={{ 
+                fontSize: FontSize 
+              }}>
+              View Details
+            </button>
+          </Link>
         </Carousel.Caption>
       </Carousel.Item>
       
