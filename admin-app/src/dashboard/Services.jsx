10254e0328b15ca5c539ca3de0f8d8704eecabf6
charithgaya <121884110+charithgaya@users.noreply.github.com>
2024-10-24 19:13:48 +0530

    changed

--- admin-app/src/dashboard/Services.jsx
+++ admin-app/src/dashboard/Services.jsx
@@ -1,13 +1,14 @@
 import React, { useState,useEffect } from 'react';
-import { Container, Button, styled, Paper, Typography, TextField,  Grid, TableCell, TableRow, tableCellClasses, TableContainer, Table, TableHead, TableBody, TablePagination, CircularProgress, IconButton } from '@mui/material';
+import { Button, CircularProgress, Container, FormControl, Grid, IconButton, InputLabel, MenuItem, Paper, Select, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TablePagination, TableRow, TextField, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import AddIcon from '@mui/icons-material/Add';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { db, storage } from '../api/firebase';
 import {fetchSelectedService,updateService,addService,fetchServices}from '../api/db/services'
+import { arrayRemove, collection, deleteDoc, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import Swal from 'sweetalert2';
-import { deleteDoc, doc } from 'firebase/firestore';
-import {useAuth} from '../api/AuthContext'
+import {useAuth} from '../api/AuthContext';
+import { serviceFilters } from '../../src/components/menuLists';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Services () {
@@ -54,10 +55,13 @@ export default function Services () {
 
 const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
   const [service, setService] = useState({
-    serviceName : '',
-    serviceDescription : [''],
-    serviceLocation : '',
+    title: '',
+    category: '',
+    subCategory: '',
+    description: [''],
+    location: '',
     images: [],
+    visibility: false
   });
 
   const [existingImages, setExistingImages] = useState([]);
@@ -134,9 +138,9 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
     const allImageUrls = [...existingImages, ...newImageUrls];
 
     if (sid) {
-      await updateService(sid, { ...service, images: allImageUrls });
+      await updateService(sid, { ...service, images: allImageUrls, visibility: false});
     } else {
-      await addService({ ...service, images: allImageUrls });
+      await addService({ ...service, images: allImageUrls, visibility: false });
     }
 
     Swal.fire({
@@ -147,10 +151,13 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
     });
 
     setService({
-      serviceName : '',
-      serviceDescription : [''],
-      serviceLocation : '',
+      title: '',
+      category: '',
+      subCategory: '',
+      description: [''],
+      location: '',
       images: [],
+      visibitlty: false
     });
 
     setExistingImages([]);
@@ -165,8 +172,7 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
       text: e.message,
     });
   }
-
-  }
+};
 
   
   const VisuallyHiddenInput = styled('input')({
@@ -185,77 +191,108 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
     <Paper style={{ padding: 16 }}>
       <Typography varient="h6">{sid ? 'Edit Service' : 'Add Service'}</Typography>
       <form onSubmit={handleSubmit}>
-        <TextField      
+        <TextField
           label="Service Name"
-          name="serviceName"
-          value={service.serviceName}
+          name="title"
+          value={service.title}
           onChange={handleChange}
           fullWidth
           margin="normal"
           required
         />
+        <FormControl fullWidth margin='normal'>
+          <InputLabel>Category</InputLabel>
+          <Select
+            name="category"
+            value={service.category}
+            onChange={handleChange}
+            required
+          >
+            {Object.keys(serviceFilters["categories"]).map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin='normal'>
+          <InputLabel>SubCategory</InputLabel>
+          <Select
+            name="subCategory"
+            value={service.subCategory}
+            onChange={handleChange}
+            required
+            disabled={!service.category}
+          >
+            {service.category &&
+              serviceFilters["categories"][service.category].map((subCategory) => (
+                <MenuItem key={subCategory} value={subCategory}>
+                  {subCategory}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
 
-        {service.serviceDescription.map((description, index) => (
+        {service.description.map((description, index) => (
           <Grid container key={index} spacing={1} alignItems="center">
-            <Grid item xs={11}>
-            <TextField
-              label= {`Description Line ${index + 1}`}
-              value={description}
-              onChange={(event) => handleDescriptionChange(index, event)}
-              fullWidth
-              margin="normal"
-              required
-            />
+            <Grid item xs={11.5}>
+              <TextField
+                label={`Description Line ${index + 1}`}
+                value={description}
+                onChange={(event) => handleDescriptionChange(index, event)}
+                fullWidth
+                margin="normal"
+                required
+              />
             </Grid>
             {index > 0 && (
-                // <Button onClick={() => handleRemoveDescriptionLine(index)} variant='outlined' color='error' startIcon={<DeleteIcon />}>
-                //   Delete
-                // </Button>
-                <IconButton onClick={() => handleRemoveDescriptionLine(index)} style={{ marginTop: '1rem' }} aria-label="delete">
-                    <DeleteIcon />
-                </IconButton>
+              <IconButton onClick={() => handleRemoveDescriptionLine(index)} style={{ marginTop: '1rem' }} aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
             )}
           </Grid>
         ))}
 
         <Button
           onClick={addDescriptionLine}
-          variant='outlined'
-          startIcon = {<AddIcon />}
-          color='success'
-          style={{ marginTop: '10px', marginBottom: '10px'}}
-        > Add new line
+          variant="outlined"
+          startIcon={<AddIcon />}
+          color="success"
+          style={{ marginTop: '10px', marginBottom: '10px' }}
+        >
+          Add new line
         </Button>
 
         <TextField
           label="Location"
-          name="serviceLocation"
-          value={service.serviceLocation}
+          name="location"
+          value={service.location}
           onChange={handleChange}
           fullWidth
           margin="normal"
           required
         />
+
         <Button
-          accept='image/*'
-          multiple
-          onChange={handleImageChange}
+          accept="image/*"
           component="label"
-          role={undefined}
-          variant="contained"
-          tabIndex={-1}
           startIcon={<CloudUploadIcon />}
-          style={{ marginTop:'15px', marginBottom: '25px' }}
+          variant="outlined"
+          style={{ marginTop: '10px', marginBottom: '10px' }}
         >
-        Upload file
-        <VisuallyHiddenInput type="file" />
+          Upload Images
+          <VisuallyHiddenInput type="file" onChange={handleImageChange} />
         </Button>
+
+        <Typography variant="body1" color="textSecondary" gutterBottom>
+          (Note:- Add high quality images.)
+        </Typography>
 
         <Grid container spacing={2}>
           {existingImages.map((src, index) => (
             <Grid item key={index}>
               <div style={{ position: 'relative' }}>
-                <img src={src} alt={`Existing Preview ${index}`} style={{ width: 150, height: 100, objectFit: 'cover' }} />
+                <img src={src} alt={`Existing Preview ${index}`} style={{ width: 150, height: 120, borderRadius: 5 ,objectFit: 'cover' }} />
                 <Button
                   onClick={() => handleRemoveImage(index, 'existing')}
                   variant="contained"
@@ -271,7 +308,7 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
           {newImages.map((file, index) => (
             <Grid item key={index + existingImages.length}>
               <div style={{ position: 'relative' }}>
-                <img src={URL.createObjectURL(file)} alt={`New Preview ${index}`} style={{ width: 150, height: 120, objectFit: 'cover' }} />
+                <img src={URL.createObjectURL(file)} alt={`New Preview ${index}`} style={{ width: 150, height: 120, borderRadius: 5 , objectFit: 'cover' }} />
                 <Button
                   onClick={() => handleRemoveImage(index, 'new')}
                   variant="contained"
@@ -285,7 +322,13 @@ const ServicesForm =  ({ sid, onSuccess, onCancel }) => {
             </Grid>
           ))}
         </Grid>
-        {validationMessage && <Typography color="error">{validationMessage}</Typography>}
+
+        {validationMessage && (
+          <Typography color="error" sx={{ mt: '1rem'}} gutterBottom>
+            {validationMessage}
+          </Typography>
+        )}
+
         <Button type="submit" variant="contained" color="success" style={{ marginTop: '25px' }}>
           Save
         </Button>
@@ -352,6 +395,7 @@ const ServiceDetails = ({ sid, onBack }) => {
 const ServicesList = ({ onEditService, onViewService }) => {
   const {user}=useAuth();
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = React.useState(true);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   
@@ -363,37 +407,38 @@ const ServicesList = ({ onEditService, onViewService }) => {
       fetchServiceList();
   }, []);
 
-  const handleDelete = async (id) => {
+  const handleDeleteService = async (serviceId) => {
+    const result = await Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    });
+
+    if (result.isConfirmed) {
       try {
-          const result = await Swal.fire({
-              icon: 'warning',
-              title: 'Are you sure?',
-              text: "You won't be able to revert this!",
-              showCancelButton: true,
-              confirmButtonText: 'Yes, delete it!',
-              cancelButtonText: 'No, cancel!',
-          });
+        await deleteDoc(doc(db, 'services', serviceId));
+        await updateDoc(doc(db,'systemusers',user.uid),{
+          myServices:arrayRemove(serviceId)
+        })
+        setServices(services.filter(service => service.sid !== serviceId));
 
-          if (result.isConfirmed){
-              await deleteDoc(doc(db, 'services', id));
-              setServices(services.filter(service => service.id !== id));
-
-              Swal.fire({
-                  icon: 'success',
-                  title: 'Deleted!',
-                  text: `The Service has been deleted.`,
-                  showConfirmButton: false,
-                  timer: 1500,
-              });
-          }
+        Swal.fire(
+          'Deleted!',
+          'Service has been deleted.',
+          'success'
+        );
       } catch (error) {
-          console.error("Error deleting service: ", error);
-          Swal.fire({
-              icon: 'error',
-              title: 'Error!',
-              text: 'There was an error deleting the service.',
-          });
+        Swal.fire({
+          icon: 'error',
+          title: 'Error deleting service',
+          text: error.message,
+        });
       }
+    }
   };
 
   const handleChangePage = (event, newPage) => {
@@ -426,50 +471,63 @@ const ServicesList = ({ onEditService, onViewService }) => {
     }));
 
     return (
-      <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                  <TableRow>
-                      {/* <TableCell>ID</TableCell> */}
-                      
-                      <StyledTableCell align="center">Service Name</StyledTableCell>
-                      <StyledTableCell align="center">Description</StyledTableCell>
-                      <StyledTableCell align="center">Location</StyledTableCell>
-                      <StyledTableCell align="center">Actions</StyledTableCell>
-                  </TableRow>
-              </TableHead>
-              <TableBody>
-                  {services.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(service => (
-                  <StyledTableRow key={service.id}>
-                      {/* <TableCell>{product.id}</TableCell> */}
-                      <StyledTableCell align="center">{service.serviceName}</StyledTableCell>
-                      <StyledTableCell>
-                          <ul style={{ textAlign: 'justify'}}>
-                              {service.serviceDescription.map((description, index) => (
-                                  <li key={index}>{description}</li>
-                              ))}
-                          </ul>
-                      </StyledTableCell>
-                      <StyledTableCell align="center">{service.serviceLocation}</StyledTableCell>
-                      <StyledTableCell align="center">
-                          <Button disabled={!user.approved} onClick={() => onViewService(service.id)} variant="outlined" color="secondary" style={{ margin: '5px', width: '100%' }}>View</Button>
-                          <Button disabled={!user.approved} onClick={() => onEditService(service.id)} variant="outlined" color="success" style={{ margin: '5px', width: '100%' }}>Edit</Button>
-                          <Button disabled={!user.approved} onClick={() => handleDelete(service.id)} variant="outlined" color="error" style={{ margin: '5px', width: '100%' }}>Delete</Button>
-                      </StyledTableCell>
-                  </StyledTableRow>
-                 ))}
-              </TableBody>
-          </Table>
-          <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={services.length} 
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-      </TableContainer>    
+      <Paper>
+      <TableContainer>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">Name</StyledTableCell>
+              <StyledTableCell align="center">Category</StyledTableCell>
+              <StyledTableCell align="center">Sub Category</StyledTableCell>
+              {/* <StyledTableCell>Description</StyledTableCell> */}
+              <StyledTableCell align="center">Location</StyledTableCell>
+              <StyledTableCell align="center">Visibility On Site</StyledTableCell>
+              <StyledTableCell align="center">Actions</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading ? (
+              <TableRow>
+                <StyledTableCell colSpan={4} align="center">
+                  <CircularProgress />
+                </StyledTableCell>
+              </TableRow>
+            ) : services.length > 0 ? (
+              services.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(service => (
+                <StyledTableRow key={service.sid}>
+                  <StyledTableCell align="center">{service.title}</StyledTableCell>
+                  <StyledTableCell align="center">{service.category}</StyledTableCell>
+                  <StyledTableCell align="center">{service.subCategory}</StyledTableCell>
+                  {/* <StyledTableCell>{service.description}</StyledTableCell> */}
+                  <StyledTableCell align="center">{service.location}</StyledTableCell>
+                  <StyledTableCell align="center">{(service.visibility === false) ? 'No':'Yes'}</StyledTableCell>
+                  <StyledTableCell>
+                    <Button onClick={() => onEditService(service.sid)} variant="outlined" color="success" style={{ margin: '5px', width: '100%' }}>Edit</Button>
+                    <Button onClick={() => onViewService(service.sid)} variant="outlined" color="secondary" style={{ margin: '5px', width: '100%' }}>View</Button>
+                    <Button onClick={() => handleDeleteService(service.sid)} variant="outlined" color="error" style={{ margin: '5px', width: '100%' }}>Delete</Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))
+            ) : (
+              <TableRow>
+                <StyledTableCell colSpan={4} align="center">
+                  No services found.
+                </StyledTableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50]}
+        component="div"
+        count={services.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 
 }
