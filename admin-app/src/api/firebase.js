a9867689fea8dfad8b1a3d48adc446467b599228
charithgaya <121884110+charithgaya@users.noreply.github.com>
2024-11-09 14:20:57 +0530

    c changeed

--- admin-app/src/api/firebase.js
+++ admin-app/src/api/firebase.js
@@ -2,8 +2,8 @@ import { connectAuthEmulator, getAuth } from 'firebase/auth';
 import firebase from 'firebase/compat/app';
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { connectStorageEmulator, getStorage } from 'firebase/storage';
-//import { getDatabase, connectDatabaseEmulator } from "firebase/database";
-//import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import { getDatabase, connectDatabaseEmulator } from "firebase/database";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNssJmBhLoe2IrCWi6Gbmy-53Y6od-9zY",
@@ -22,19 +22,19 @@ const auth = getAuth();
 const storage = getStorage();
 //const realtimeDB = getDatabase()
 
-/*
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6Ld8mQkqAAAAADb9fn9eJaoq4mAkcZ0SKA8-5sbE'),
-  isTokenAutoRefreshEnabled: true
-});
-*
+
+// const appCheck = initializeAppCheck(app, {
+//   provider: new ReCaptchaV3Provider('6Ld8mQkqAAAAADb9fn9eJaoq4mAkcZ0SKA8-5sbE'),
+//   isTokenAutoRefreshEnabled: true
+// });
+
 
 if (window.location.hostname === "localhost" || window.location.hostname === '127.0.0.1' || window.location.hostname === '192.168.70.247') {
   connectFirestoreEmulator(db, '127.0.0.1', 8080);
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
   connectStorageEmulator(storage, '127.0.0.1', 9199);
   //connectDatabaseEmulator(realtimeDB, '127.0.0.1', 9000);
-}*/
+}
 
 
 export { db, auth, storage }
