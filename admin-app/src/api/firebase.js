ade5d03074fefec3b2f12ed9018d50d71bfc0c3e
SachinthaWijerathne <tharindu.sachintha.xyz1@gmail.com>
2024-12-19 06:27:53 +0530

    testing

--- admin-app/src/api/firebase.js
+++ admin-app/src/api/firebase.js
@@ -4,13 +4,13 @@ import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { connectStorageEmulator, getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNssJmBhLoe2IrCWi6Gbmy-53Y6od-9zY",
-  authDomain: "yarlprops.firebaseapp.com",
-  projectId: "yarlprops",
-  storageBucket: "yarlprops.appspot.com",
-  messagingSenderId: "705284763370",
-  appId: "1:705284763370:web:b2655e35aa3ce698c6b193",
-  measurementId: "G-ZRR85H89Z4"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 //const app = initializeApp(firebaseConfig)
